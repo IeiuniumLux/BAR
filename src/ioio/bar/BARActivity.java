6f114fb60dfa97d0cf421373bbd1c1034ad74ced
Al B <cagiva851@gmail.com>
2014-10-26 14:56:02 -0700

    Read UART input for TouchOSC

--- src/ioio/bar/BARActivity.java
+++ src/ioio/bar/BARActivity.java
@@ -24,9 +24,13 @@
 
 package ioio.bar;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import ioio.lib.api.DigitalOutput;
 import ioio.lib.api.IOIO;
 import ioio.lib.api.Sequencer;
+import ioio.lib.api.Uart;
 import ioio.lib.api.exception.ConnectionLostException;
 import ioio.lib.util.BaseIOIOLooper;
 import ioio.lib.util.IOIOLooper;
@@ -53,7 +57,7 @@ public class BARActivity extends IOIOActivity implements SensorEventListener {
 	// Declares which types of channels we are going to use and which pins they should be mapped to. The order of the channels
 	// in this array is important because it is used to define cues for those channels in the Sequencer.ChannelCue[] array.
 	// ---
-	private Sequencer.ChannelConfig[] channel_config = { 
+	private Sequencer.ChannelConfig[] _channelConfig = { 
 			new Sequencer.ChannelConfigFmSpeed(Sequencer.Clock.CLK_62K5, 2, new DigitalOutput.Spec(7)),  // LEFT STEP
 			new Sequencer.ChannelConfigBinary(false, false, new DigitalOutput.Spec(18)), 				 // LEFT DIR
 			new Sequencer.ChannelConfigFmSpeed(Sequencer.Clock.CLK_62K5, 2, new DigitalOutput.Spec(13)), // RIGHT STEP
@@ -63,19 +67,19 @@ public class BARActivity extends IOIOActivity implements SensorEventListener {
 	// ---
 	// FM (frequency modulation) speed channels are useful for driving stepper motors in speed control mode (i.e. how fast it's moving).
 	// ---
-	private Sequencer.ChannelCueFmSpeed left_steps = new Sequencer.ChannelCueFmSpeed();
-	private Sequencer.ChannelCueFmSpeed right_steps = new Sequencer.ChannelCueFmSpeed();
+	private Sequencer.ChannelCueFmSpeed _leftSteps = new Sequencer.ChannelCueFmSpeed();
+	private Sequencer.ChannelCueFmSpeed _rightSteps = new Sequencer.ChannelCueFmSpeed();
 	
 	// ---
 	// A cue binary channel is to drive the pin "low" "high".
 	// ---
-	private Sequencer.ChannelCueBinary left_dir = new Sequencer.ChannelCueBinary();
-	private Sequencer.ChannelCueBinary right_dir = new Sequencer.ChannelCueBinary();
+	private Sequencer.ChannelCueBinary _leftDir = new Sequencer.ChannelCueBinary();
+	private Sequencer.ChannelCueBinary _rightDir = new Sequencer.ChannelCueBinary();
 	
 	// ---
 	// The order and type of elements in this array must match the Sequencer.ChannelConfig[] array.
 	// ---
-	private Sequencer.ChannelCue[] channel_cue = { left_steps, left_dir, right_steps, right_dir };
+	private Sequencer.ChannelCue[] _channelCue = { _leftSteps, _leftDir, _rightSteps, _rightDir };
 	
 	/*
 	M0     -> IOIO:  3 / Shield: 3
@@ -91,16 +95,16 @@ public class BARActivity extends IOIOActivity implements SensorEventListener {
 	DIR    -> IOIO: 14 / Shield: 13
 	 */
 	
-	private int[] left_pins = { 3, 24, 6 };
-	private int[] right_pins = { 10, 11, 12 };
+	private int[] _leftPins = { 3, 24, 6 };
+	private int[] _rightPins = { 10, 11, 12 };
 
-	private PowerManager.WakeLock wake_lock;
-	private SensorManager sensor_manager;
-	private Sensor rotation_vector_sensor;
+	private PowerManager.WakeLock _wakeLock;
+	private SensorManager _sensorManager;
+	private Sensor _rotationVectorSensor;
 
-	GestureDetector gesture_detector;
-	SharedPreferences shared_preferences;
-	static int offset_index = 2;
+	GestureDetector _gestureDetector;
+	SharedPreferences _sharedPreferences;
+	static int _offsetIndex = 2;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -108,16 +112,16 @@ public class BARActivity extends IOIOActivity implements SensorEventListener {
 		setContentView(R.layout.main);
 		
 		PreferenceManager.setDefaultValues(this, R.xml.settings, false);
-		shared_preferences = PreferenceManager.getDefaultSharedPreferences(this);
-		offset_index = Integer.parseInt(shared_preferences.getString(getString(R.string.color_key), "0"));
+		_sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+		_offsetIndex = Integer.parseInt(_sharedPreferences.getString(getString(R.string.color_key), "0"));
 		
 		PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-		wake_lock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "BAR");
-		sensor_manager = (SensorManager) getSystemService(SENSOR_SERVICE);
-		rotation_vector_sensor = sensor_manager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-		last_timestamp = 0;
+		_wakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "BAR");
+		_sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		_rotationVectorSensor = _sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+		_lastTimestamp = 0;
 		
-		gesture_detector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+		_gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
 			@Override
 			public void onLongPress(MotionEvent e) {
 				startActivityForResult(new Intent(getApplicationContext(), SettingsActivity.class), 0);
@@ -128,45 +132,45 @@ public class BARActivity extends IOIOActivity implements SensorEventListener {
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
-		return gesture_detector.onTouchEvent(event);
+		return _gestureDetector.onTouchEvent(event);
 	}
 	
 	@Override
 	protected void onStart() {
 		super.onStart();
-		wake_lock.acquire();
+		_wakeLock.acquire();
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		sensor_manager.registerListener(this, rotation_vector_sensor, SensorManager.SENSOR_DELAY_GAME);
-		last_timestamp = 0;
-		last_error = 0;
+		_sensorManager.registerListener(this, _rotationVectorSensor, SensorManager.SENSOR_DELAY_GAME);
+		_lastTimestamp = 0;
+		_lastError = 0;
 		hideNavigationBar();
 	}
 	
 	@Override
 	protected void onStop() {
-		wake_lock.release();
-		sensor_manager.unregisterListener(this);
+		_wakeLock.release();
+		_sensorManager.unregisterListener(this);
 		super.onStop();
 	}
 	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		offset_index = Integer.parseInt(shared_preferences.getString(getString(R.string.color_key), "2"));
+		_offsetIndex = Integer.parseInt(_sharedPreferences.getString(getString(R.string.color_key), "2"));
 	}
 
 	private class DRV8834 {
-		private final Sequencer.ChannelCueBinary dir;
-		private final Sequencer.ChannelCueFmSpeed step;
-		private final DigitalOutput sleep;
+		private final Sequencer.ChannelCueBinary _dir;
+		private final Sequencer.ChannelCueFmSpeed _step;
+		private final DigitalOutput _sleep;
 
 		public DRV8834(IOIO ioio, int[] pins, Sequencer.ChannelCueFmSpeed step, Sequencer.ChannelCueBinary dir) throws ConnectionLostException {
-			this.dir = dir;
-			this.step = step;
+			this._dir = dir;
+			this._step = step;
 			step.period = 0;
 
 			// NOTE: The default state of the ENBL pin is to enable the driver, so this pin can be left disconnected.
@@ -174,64 +178,90 @@ public class BARActivity extends IOIOActivity implements SensorEventListener {
 			ioio.openDigitalOutput(pins[0], true); // M0
 			ioio.openDigitalOutput(pins[1], true); // M1
 			
-			sleep = ioio.openDigitalOutput(pins[2], false);
+			_sleep = ioio.openDigitalOutput(pins[2], false);
 		}
 
 		public void setEnable(boolean en) throws ConnectionLostException {
-			sleep.write(en);
+			_sleep.write(en);
 		}
 		
 		public void setSpeed(float speed) throws ConnectionLostException {
-			dir.value = (speed > 0);
+			_dir.value = (speed > 0);
 			speed = Math.abs(speed) * 10000;
-			if (speed < 40) speed = 40;
-			if (speed > 3600) speed = 3600;
-			step.period = Math.round(STEPS_FREQ / speed);
+			if (speed < 50) speed = 50;
+			if (speed > 3620) speed = 3620;
+			_step.period = Math.round(STEPS_FREQ / speed);
 		}
 	}
 	
 	
 	class BalancerLooper extends BaseIOIOLooper {
-		private final DRV8834[] motors = new DRV8834[2];
+		private final DRV8834[] _motors = new DRV8834[2];
 		private static final int SLEEP_MS = 2;
-		private Sequencer sequencer;
+		private Sequencer _sequencer;
+		
+		private Uart _uart;
+		private InputStream _uartInput;
+		
+		int _inByte = 0;      // in-comming serial byte
+		int _inbyteIndex = 0; // in-comming bytes counter
+		char _oscControl;     // control in TouchOSC sending the message
+		int[] _oscMsg = new int[11]; // buffer for incoming OSC packet
 
 		@Override
 		public void setup() throws ConnectionLostException {
-			last_timestamp = 0;	
-			integrated_error = 0;
-			motors[0] = new DRV8834(ioio_, left_pins, left_steps, left_dir);
-			motors[1] = new DRV8834(ioio_, right_pins, right_steps, right_dir);
-			sequencer = ioio_.openSequencer(channel_config);
+			_lastTimestamp = 0;	
+			_integratedError = 0;
+			_motors[0] = new DRV8834(ioio_, _leftPins, _leftSteps, _leftDir);
+			_motors[1] = new DRV8834(ioio_, _rightPins, _rightSteps, _rightDir);
+			_sequencer = ioio_.openSequencer(_channelConfig);
+			
+			_uart = ioio_.openUart(5, 4, 9600, Uart.Parity.NONE, Uart.StopBits.ONE);
+			_uartInput = _uart.getInputStream();
 		}
 
 		@Override
 		public void loop() throws ConnectionLostException {
 			try {
+				_inByte = _uartInput.read();
+				
 				float speed = 0;
-			    if (tilt_angle < BALANCE_LIMIT && tilt_angle > -BALANCE_LIMIT) {
-			    	speed = control_output;
-			        motors[0].setEnable(true);
-					motors[1].setEnable(true);
+			    if (_tiltAngle < BALANCE_LIMIT && _tiltAngle > -BALANCE_LIMIT) {
+			    	speed = _controlOutput;
+			        _motors[0].setEnable(true);
+					_motors[1].setEnable(true);
 			    } else  {	
-			    	motors[0].setEnable(false);
-					motors[1].setEnable(false);
-					last_error = 0;
-					sequencer.manualStop();
+			    	_motors[0].setEnable(false);
+					_motors[1].setEnable(false);
+					_lastError = 0;
+					_sequencer.manualStop();
 			    }
-			    motors[0].setSpeed(-speed);
-				motors[1].setSpeed(speed);
-				sequencer.manualStart(channel_cue);
+			    _motors[0].setSpeed(-speed);
+				_motors[1].setSpeed(speed);
+				_sequencer.manualStart(_channelCue);
 				
 				Thread.sleep(SLEEP_MS);
 			} catch (InterruptedException e) {
 				ioio_.disconnect();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 
 		@Override
 		public void disconnected() {
-			sequencer.close();
+			_sequencer.close();
+			try {
+				if (_uartInput != null)
+					_uartInput.close();
+				if (_uart != null)
+					_uart.close();
+			} catch (IOException e) {
+				// Nothing to do at this point!
+			} finally {
+				_uartInput = null;
+				_uart = null;
+			}
 		}
 	}
 
@@ -244,52 +274,52 @@ public class BARActivity extends IOIOActivity implements SensorEventListener {
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 	}
 	
-	private float[] offset = { 
-			0.0872664626f, // [0] 5º
-			0.0610865238f, // [1] 3.5º
-			0.0523598776f, // [2] 3º
-			0.0436332313f, // [3] 2.5º
-			0.0174532925f, // [4] 1º
+	private float[] _offset = { 
+			0.0829031395f, // [0] 4.75º
+			0.0174532925f, // [1] 1º
+			0.0130899694f, // [2] 0.75º
+			0.00872664626f,// [3] 0.5º
+			0.00436332313f // [4] 0.25º
 	};
 	
-	private static final float BALANCE_LIMIT = 0.698131701f;  //40 degrees
+	private static final float BALANCE_LIMIT = 0.872664626f;  // Shutdown motors @ 50º 
 	
-	private float kP = 1.7f;
-	private float kI = 12.2f;
-	private float kD = 0.5f;
+	private float _kP = 1.49f;
+	private float _kI = 13.9f;
+	private float _kD = 0.35f;
 	
-	private volatile float tilt_angle = 0.0f;
-	private long last_timestamp = 0;	
-	private float integrated_error = 0;
-	private float last_error = 0.0f;
-	private volatile float control_output = 0.0f;
+	private volatile float _tiltAngle = 0.0f;
+	private long _lastTimestamp = 0;	
+	private float _integratedError = 0;
+	private float _lastError = 0.0f;
+	private volatile float _controlOutput = 0.0f;
 			
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
-			if (last_timestamp != 0) {
-				long dT = event.timestamp - last_timestamp;
+			if (_lastTimestamp != 0) {
+				long dT = event.timestamp - _lastTimestamp;
 				float[] quaternion = new float[4];
 
 				SensorManager.getQuaternionFromVector(quaternion, event.values);
 		        
 				// Roll-Tilt-Angle (landscape mode - 90º degree raised up)
-		        tilt_angle = (float)((Math.asin(quaternion[0] * quaternion[0] - quaternion[1] * quaternion[1] - quaternion[2] * quaternion[2] + 
-		        		quaternion[3] * quaternion[3]) - offset[offset_index]));
+		        _tiltAngle = (float)((Math.asin(quaternion[0] * quaternion[0] - quaternion[1] * quaternion[1] - quaternion[2] * quaternion[2] + 
+		        		quaternion[3] * quaternion[3]) - _offset[_offsetIndex]));
 		        
-		        control_output = pidController(-1 * (tilt_angle * 0.99f), tilt_angle, kP, kI, kD, dT);
+		        _controlOutput = pidController(-1 * (_tiltAngle * 0.99f), _tiltAngle, _kP, _kI, _kD, dT);
 			}
-			last_timestamp = event.timestamp;
+			_lastTimestamp = event.timestamp;
 		}
 	}
 	
 	private float pidController(float setpoint, float input, float kP, float kI, float kD, float dT) {
 		float error = setpoint - input;
-		integrated_error += 0.99f * error; // low-pass IIR filter
-		integrated_error = constrain(integrated_error, -1, 1);
-		float derivative = error - last_error;
-		last_error = error;
-		return (kP * error + kI * (integrated_error * dT * 1e-9f) + kD * (derivative / dT * 1e-9f));
+		_integratedError += 0.99f * error; // low-pass IIR filter
+		_integratedError = constrain(_integratedError, -1, 1);
+		float derivative = error - _lastError;
+		_lastError = error;
+		return (kP * error + kI * (_integratedError * dT * 1e-9f) + kD * (derivative / dT * 1e-9f));
 	}
 
 	private void hideNavigationBar() {
