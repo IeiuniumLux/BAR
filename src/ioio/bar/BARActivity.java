a5f3daf3bd1653129e02568968670844be3a46dd
abencomo <Alfredo.Bencomo@nasa.gov>
2015-05-14 20:41:57 -0700

    Set the content to appear under the system bars so that the content doesn't resize when the system bars hide and show.

--- src/ioio/bar/BARActivity.java
+++ src/ioio/bar/BARActivity.java
@@ -113,7 +113,11 @@ public class BARActivity extends IOIOActivity implements SensorEventListener, UD
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+	            | View.SYSTEM_UI_FLAG_FULLSCREEN);
 		return _gestureDetector.onTouchEvent(event);
 	}
 
@@ -427,7 +431,11 @@ public class BARActivity extends IOIOActivity implements SensorEventListener, UD
 	}
 
 	private void hideNavigationBar() {
-		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+	            | View.SYSTEM_UI_FLAG_FULLSCREEN);
 	}
 
 	private float constrain(final float value, final float min, final float max) {
